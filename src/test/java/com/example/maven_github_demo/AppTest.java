cdae1f46b139fa8bbb9628f3f89d4a59929ee6ff
nedit23 <thejusparihar@gmail.com>
2026-09-23 16:11:08 +0530

    workflow error

--- src/test/java/com/example/maven_github_demo/AppTest.java
+++ src/test/java/com/example/maven_github_demo/AppTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
 	@Test
     void testTotal() {
-        assertEquals(225,
+        assertEquals(225000000,
             App.calculateTotal(75, 68, 82));
     }
 
